50191138b4846a183eb10ac1486148969ffb2224
Ruth Poliakon <r.poliakon@gmail.com>
2019-08-22 20:13:14 -0600

    button components created and imported

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { numbers } from "../../../data";
 
-const Numbers = () => {
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbers.map((item, index) => {
+        return <button key={index}>{item}</button>;
+      })}
     </div>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { specials } from "../../../data";
 
-const Specials = () => {
+export const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {specials.map((item, index) => {
+        return <button key={index}>{item}</button>;
+      })}
     </div>
   );
 };
